643a9eded4f7d8be3a9405afd1dc1755fb4ecade
chihiro <c28hero@gmail.com>
2016-11-16 13:23:44 +0900

    ReStableを導入します。 refs @0.5h

---
+++ MvcExperiment.GridMvc/Controllers/RestableController.cs
@@ -0,0 +1,25 @@
+﻿namespace MvcExperiment.GridMvc.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using MvcExperiment.GridMvc.Models;
+    using MvcExperiment.GridMvc.Services;
+
+    /// <summary>
+    /// Restable
+    /// </summary>
+    public class RestableController : Controller
+    {
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult Index()
+        {
+            var model = CaractorService.GetCaractors().OrderBy(m => m.ID).ToList();
+
+            return this.View(model);
+        }
+    }
+}
